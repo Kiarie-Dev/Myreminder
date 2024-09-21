e77b69ed8c049ca9a423c011493a3bff2c6d2534
dataNerd65 <pkiarie811@gmail.com>
2024-09-21 20:34:39 +0300

    Some mods

--- Taskmanager.py
+++ Taskmanager.py
@@ -4,10 +4,11 @@ from datetime import datetime
 class TaskManager:
     def __init__(self, name: str):
         self.name = name
-        
+
     def start_up(self):
         current_date = datetime.now().strftime("%Y-%m-%d")
         print(f"Today is {current_date}. What are you planning, {self.name}?")
+        
 
 if __name__ == "__main__":
     task_manager = TaskManager()
