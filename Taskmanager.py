8a0340334ea7a68742ff899daa5fa0f4ce393259
dataNerd65 <pkiarie811@gmail.com>
2024-09-23 19:32:48 +0300

    Added task options

--- Taskmanager.py
+++ Taskmanager.py
@@ -7,6 +7,10 @@ from typing import List, Dict
 # Constants
 ERROR_READING_FILE_MSG = "Error reading tasks file. Starting with an empty task list!"
 TASKS_FILE = 'tasks.json'
+ERROR_SAVING_TASKS_MSG = "Error saving tasks!"
+INCOMPLETE_TASKS_MSG = "You have incomplete tasks:"
+NO_TASKS_MSG = "You have no tasks."
+INVALID_CHOICE_MSG = "Invalid choice. Please try again."
 
 
 class TaskManager:
@@ -27,10 +31,45 @@ class TaskManager:
     
     def save_tasks(self) -> None:
         """Save tasks to a JSON file."""
+        try:
+            with open(TASKS_FILE, 'w') as file:
+                json.dump(self.talks, file, indent=4)
+        except IOError:
+            print(ERROR_SAVING_TASKS_MSG)
+
 
     def start_up(self):
+        """Start the application and present options to the user."""
         current_date = datetime.now().strftime("%Y-%m-%d")
         print(f"Today is {current_date}. What are you planning, {self.name}?")
+
+        if self.tasks:
+            print(INCOMPLETE_TASKS_MSG)
+            self.view_tasks(only_incomplete=True)
+        else:
+            print(NO_TASKS_MSG)
+        
+        while True:
+            print("\nOptions:")
+            print("1. View all tasks")
+            print("2. Add a new task")
+            print("3. Delete a task")
+            print("4. Mark a task as complete")
+            print("5. Exit")
+            choice = input("Select an option: ")
+
+            if choice == '1':
+                self.view_tasks()
+            elif choice == '2':
+                self.add_task()
+            elif choice == '3':
+                self.delete_task()
+            elif choice == '4':
+                self.mark_task_complete()
+            elif choice == '5':
+                break
+            else:
+                print(INVALID_CHOICE_MSG)
         
 
 if __name__ == "__main__":
