4b4e7947b601b36ac5797f2f31a4f1965f14305e
dataNerd65 <pkiarie811@gmail.com>
2024-09-23 19:15:31 +0300

    Added fns for file operations

--- Taskmanager.py
+++ Taskmanager.py
@@ -1,9 +1,32 @@
 from datetime import datetime
+import os
+import json
+from typing import List, Dict
+
+
+# Constants
+ERROR_READING_FILE_MSG = "Error reading tasks file. Starting with an empty task list!"
+TASKS_FILE = 'tasks.json'
 
 
 class TaskManager:
     def __init__(self, name: str):
         self.name = name
+        self.tasks = self.load_tasks()
+    
+    def load_tasks(self) -> List[Dict[str, str]]:
+        """Loading files from a JSON file."""
+        if os.path.exists(TASKS_FILE):
+            try:
+                with open(TASKS_FILE, 'r') as file:
+                    return json.load(file)
+            except json.JSONDecodeError:
+                print(ERROR_READING_FILE_MSG)
+                return []
+            return []
+    
+    def save_tasks(self) -> None:
+        """Save tasks to a JSON file."""
 
     def start_up(self):
         current_date = datetime.now().strftime("%Y-%m-%d")
